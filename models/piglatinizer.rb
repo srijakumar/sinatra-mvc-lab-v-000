014af5334ac6b006c06e001ab14a18f2f66dc193
Srija S <srijakumar7@gmail.com>
2018-10-04 15:28:39 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,46 +1,45 @@
 class PigLatinizer
 
-                def piglatinize(input)
-                  if input.split(" ").length == 1
-                    pig_word(input)
-                  else
-                    pig_sentence(input)
-                  end
+  def piglatinize(input)
+      if input.split(" ").length == 1
+          pig_word(input)
+      else
+          pig_sentence(input)
+      end
+ end
 
-                end
+  def pig_word(words)
+    words = words.split("")
+    words.each do |word|
+      vowels = %w{a e i o u}
 
-                    def pig_word(word)
-                      words.each do |word|
-                        vowels = %w{a e i o u}
+    if vowels.include? words[0]
+        result.push words << 'ay'
+    else
+        helper(words)
+    end
 
-                          if vowels.include? word[0]
-                            result.push word << 'ay'
-                          else
-                            helper(word)
-                          end
+    end
+  end
 
-                          def helper(word)
-                            if !(vowels.include? word[0]) && !(vowels.include? word[1]) && !(vowels.include? word[2]) # word starts with 3 consonants
-                                word = word.slice(3..-1) + word.slice(0,3)
-                                  # word starts with 2 consonants
-                                      elsif !(vowels.include? word[0]) && !(vowels.include? word[1])
-                                  #Example: though
-                                        word = word.slice(2..-1) + word.slice(0,2)
-                                  #word = ough + th
-                                  #word = oughth
-                                  # word starts with 1 consonant
-                                        else
-                                          word = word.slice(1..-1) + word.slice(0)
-                                        end
-                                      word << "ay"
-                                #word = oughthay
-                          end
+  def helper(words)
+    if !(vowels.include? words[0]) && !(vowels.include? words[1]) && !(vowels.include? words[2]) # word starts with 3 consonants
+        words = words.slice(3..-1) + words.slice(0,3)
+        # word starts with 2 consonants
+    elsif !(vowels.include? words[0]) && !(vowels.include? words[1])
+        #Example: though
+        words = words.slice(2..-1) + words.slice(0,2)
+        #word = ough + th
+        #word = oughth
+        # word starts with 1 consonant
+    else
+        words = words.slice(1..-1) + words.slice(0)
+    end
+    words << "ay"
+  end
 
-                          def pig_sentence(sentence)
-                              sentence.split.collect { |word| pig_word(word) }.join(" ")
-                          end
-
-                          end
+def pig_sentence(sentence)
+    sentence.split.collect { |word| pig_word(word) }.join(" ")
 end
 
 end
